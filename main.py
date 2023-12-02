a74019d45658997289ce1b9bba7a3b18b91d27ba
henrikandre <56063655+henrikandre@users.noreply.github.com>
2023-12-02 20:18:13 +0100

    fix for flask

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
 
     client = base.Client(('localhost', 11211))  # Replace with your Memcached server address
 
-    key = 'example_key'
+    key = 'feature_flag'
 
     # Retrieve the value set by Client 1
     result = client.get(key)
@@ -18,14 +18,15 @@ def index():
     print(f'Client 2 - Value from Memcached: {result}')
 
     # HTML-formatted text
-    html_text1 = "<h1>Hello, Feature flag 1! Is there a key here "+{result}+"</h1><p>This is a sample HTML-formatted text.</p>"
+    html_text = "<h1>Hello, Feature flag 1! Is there a key here "+{result}+"</h1><p>This is a sample HTML-formatted text.</p>"
     html_text2 = "<h1>Hello, Feature flag 2!</h1><p>This is a sample HTML-formatted text.</p>"
 
     # Render the HTML template
-    if feature_flag:
-        return render_template('index.html', html_text=html_text1)
-    else:
-        return html_text2
+    # if feature_flag:
+    #     return render_template('index.html', html_text=html_text1)
+    # else:
+    #     return html_text2
+    return html_text
 
 
 if __name__ == '__main__':
